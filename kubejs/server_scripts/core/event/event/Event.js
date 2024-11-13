bba5ff5d3879859825c19242f3950daa290e3999
Nagasaki Soyo <2734970539@qq.com>
2024-11-13 22:46:30 +0800

    删除无用的Events.js 将$Event真正变的具有继承意义

--- kubejs/server_scripts/core/event/event/Event.js
+++ kubejs/server_scripts/core/event/event/Event.js
@@ -1,11 +1,25 @@
 /**
  * @class
- * @classdesc 事件基类 尽管一些参数类型标明需要$Event及其子类 但不会真的进行Instanceof判断
+ * @classdesc 事件基类 尽管一些参数类型标明需要$Event及其子类 但不会真的进行Instanceof判断(过时消息)
+ * @description 最新消息 已经转向支持继承的方向修改
  */
 function $Event() {
+  /**
+   * @type {boolean} 可取消的 
+   */
+  this.cancelable = false;
+  /**
+   * @type {boolean} 已取消
+   */
   this.canceled = false;
-
 }
-$Event.prototype = {
 
+/**
+ * 尝试取消事件 如果事件是Cancelable的 就会取消成功
+ * @returns {boolean} 已经取消?
+ */
+$Event.prototype.cancel = function () {
+  if (this.cancelable) {
+    this.canceled = true;
+  }
 }
